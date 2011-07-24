4791e81d3b8a13bacb8b51078a2277408d385649
3104524 <bclu.trip@gmail.com>
2011-07-24 08:34:02 -0700

    Edited public_html/index.php via GitHub

--- public_html/index.php
+++ public_html/index.php
@@ -1,125 +1,19 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html lang="ja"> 
-<head> 
- 
-<meta http-equiv="Content-Type" content="text/html; charset=utf8"> 
-<link rel="stylesheet" type="text/css" href="body.css"> 
- 
-<title>めし☆なに</title> 
- 
-</head> 
-<body> 
- 
-<!-- コンテナ開始 --> 
-<div id="container"> 
- 
- 
-<!-- ヘッダ開始 --> 
-<div id="header"> 
-<a href="index.html"><img src="logo.png" alt="めし☆なに"></a> 
-</div> 
-<!-- ヘッダ終了 --> 
- 
- 
-<!-- コンテンツ開始 --> 
-<div id="content"> 
-<div id="cen"> 
-<table> 
-<th class ="index"> 
-<form action="home.php" method="post"> 
-<p> 
-Google ID:<input type="text" name="id" value="meshi.nani"> 
-<input type="submit" value="家族の食事ログへ"> 
-</p> 
-</form> 
- 
-<h2 class="help">お母さんの「夕飯何にしましょ…」を助けます！</h2> 
-<form action="home.php" method="post"> 
-<div id ="gray"> 
-<p class="gray">"めし☆なに"を使えば、一週間分の家族の食事を確認でき、</p> 
-<p class="gray">オススメレシピを提案することでこんな母親の悩みを解決します。</p> 
-</div> 
-<br/> 
- 
-<p> 
- 
-<a href="http://twitter.com/meshinani" target="_blank"><img class="bird" src="http://twitbtn.com/images/buttons/button_new120_05.png" alt="Twitterボタン" border="0" /></a> 
- 
-※初めての方はここをクリック→ <a href="prepare.php" target="_blank"><img src="img/register.jpg"/></
- 
-</p><br /> 
-</th> 
-<th> 
- 
-<!-- start twitterwind code --> 
-<script type="text/javascript"> 
-<!--
-var twitterwind_option     = '{"isOnlyMe":"false","twitterwind_logo":"none","twitterwind_logo_bgcolor":"FFFFFF","twitterwind_logo_color":"333303","followers_color":"000000","followers_bgcolor":"FFFFFF","twitterwind_max_length":"39","twitterwind_username":"table-row","twitterwind_username_color":"333333","twitterwind_username_bgcolor":"FFFFFF","twitterwind_twit":"block","twitterwind_twit_color":"333333","twitterwind_twit_bgcolor":"FFFFFF","twitterwind_twit_link_color":"0084B4","twitterwind_twit_scroll_color":"C0DEED","twitterwind_twit_scroll_bg_color":"FFFFFF","twitterwind_follower":"none","twitterwind_follower_bgcolor":"FFFFFF","twitterwind_frame_width":160,"twitterwind_frame_height":320,"twitterwind_frame_border":"false","twitterwind_frame_border_color":"C0DEED","twitterwind_base_font_size":"12","twitterwind_me_font_size":14,"twitterwind_caption_font_size":16,"twitterwind_scroll_height":272,"eusn":"ipfWTxykbp0="}';
-var twitterwind_url        = 'http://www.tweetswind.com/twitterwind.php';
-//-->
-</script> 
-<script type="text/javascript" src="http://www.tweetswind.com/js/twitterwind.js"></script> 
-<!--利用規約に従ってページ内に必ずリンクを表示してください--> 
-<div style="font-size:12px; text-align:right; width:160px"><a target="_blank" href="http://www.tweetswind.com">Twitter ブログパーツ</a></div> 
-<!-- end twitterwind code --> 
-    <!--da4436684850f8409cbafa3614e826cb--> 
- 
- 
-</th> 
-</table> 
-</div> 
-</div> 
-<!-- コンテンツ終了 --> 
- 
- 
-<!-- フッタ開始 --> 
-<div id="footer"> 
- 
-<table><tr><td> 
-<br /> 
-<b>Notice</b>:  Undefined offset:  2 in <b>/home/cookpad2/htdocs/public_html/reccomend.php</b> on line <b>38</b><br /> 
-<br /> 
-<b>Notice</b>:  Undefined offset:  2 in <b>/home/cookpad2/htdocs/public_html/reccomend.php</b> on line <b>38</b><br /> 
-<marquee width="570" direction="left" scrollamount="5" bgcolor="#ffdddd"> 
-<font color="#ff0000" size="5"> 
-「夕飯何にしましょ…」とはオサラバ。お母さんを助けましょう！　　母:「毎日の夕食の献立考えるの大変だわ…、今日はカレーにしましょう。」（コトコト…）　　次男「ただいま～、今日の給食カレーだった」　　母「えー!?」</font></td><td> 
-<!-- Begin COOKPAD Search Form --> 
-<script type="text/JavaScript"><!--
-clear_cplogo = function( self ){
-self.value = "";
-self.style.color = "#000";
-self.onfocus = null;
-};
-search_cplogo = function( self ){
-window.open( 'http://cookpad.com/search/?keyword=' + encodeURIComponent( self.elements[ 'keyword' ].value) + '&utm_source=link_search&utm_medium=link_banner&utm_content=search150x60&utm_term=' + encodeURIComponent( self.elements[ 'keyword' ].value) );
-};
-//-->
-</script> 
- 
-<form onSubmit="search_cplogo( this );return false;"> 
-<table width="200" height="70" border="0" cellpadding="0" cellspacing="0" background="http://img2.cookpad.com/image/link/bg20070.gif"> 
-<tr><td> 
-<a href="http://cookpad.com/?utm_source=link_search&utm_medium=link_banner&utm_content=logo" target="_blank"><img src="http://img2.cookpad.com/image/link/cookpadlogo_l.gif" alt="クックパッド" width="136" height="33" style="margin-top: 2px; margin-left: 4px; border: 0;"></a> 
-</td></tr> 
-<tr> <td align="center"> 
-<input id="keyword" name="keyword" type="text" value="レシピ検索" style="font-size: 10px; color: #666; width: 130px; margin-bottom:2px;" onFocus="clear_cplogo( this );"> 
-<input type="image" src="http://img2.cookpad.com/image/link/btn.gif" alt="検索" width="46" height="21" style="margin-bottom:2px;" align="absmiddle"> 
-</td></tr> 
-</table></form> 
-<!-- End COOKPAD Search Form --> 
-</td> 
- 
- 
- 
- 
-</div> 
-<!-- フッタ終了 --> 
- 
- 
-</div> 
-<!-- コンテナ終了 --> 
- 
-</body> 
-</html> 
-<?php
-  //phpinfo();
+<html>
+<body>
+　　　　,,,,,,,,,,,,,,,,,,,,　
+　　　 ||||||||||||||||||ii　
+　　　 ,,||||||||||||||||||||
+　　 i||||||||||||||||||||||||||||i　
+　 i|||||||||||||||||||||||||||||||||||||i　
+　|||||||||||||||||||||||||||||||||||　!||.　
+　|━-　 -━━━　||||||||　|||
+　,・=-|　 -=・=- 　 /￣||| ,||||　
+　|　 /　　　　　　 　　_!|||||!!　
+. |　（＿,　　　　　　￣||||| 　
+　|　━__━　　　　　　|||| 
+　 ヽ､ ｀~　,,,;;;;;;;__　　 ||||　
+　　　||||||||||||　
+　　　 !|||||||!!　
+　　　　!||||!　
+</body>
+</html>
