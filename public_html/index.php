0333ce4ef25b9ba522a4962cbac7d1ae1f3d69d4
3104524 <bclu.trip@gmail.com>
2011-09-01 14:58:01 +0900

    Edited public_html/index.php via GitHub

--- public_html/index.php
+++ public_html/index.php
@@ -5,13 +5,13 @@
 <html>
 <body>
 <p>就活攻略</p>
-<table border="10" width="500" cellspacing="5" cellpadding="5" bordercolor="#ffffff">
+<table border="5" width="500" cellspacing="5" cellpadding="5" bordercolor="#ffffff">
 <tr>
-<td bgcolor="#fff4ea" valign="top" >カルボナーラ</td>
+<td bgcolor="#fff4ea" valign="top" width="250" height="100">カルボナーラ</td>
 <td bgcolor="#fff4ea" valign="top" width="250" height="100">玉子とベーコンとクリームソースのパスタ</td>
 </tr>
 <tr>
-<td bgcolor="#fff4ea" valign="top" >ペスカトーレ</td>
+<td bgcolor="#fff4ea" valign="top" width="250" height="100">ペスカトーレ</td>
 <td bgcolor="#fff4ea" valign="top" width="250" height="100">エビとあさりの漁師風パスタ</td>
 </tr>
 </table>
