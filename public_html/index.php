079552f8af27ef12a92110e5e30ecd2dde8b421e
3104524 <bclu.trip@gmail.com>
2011-09-01 14:54:54 +0900

    Edited public_html/index.php via GitHub

--- public_html/index.php
+++ public_html/index.php
@@ -5,17 +5,17 @@
 <html>
 <body>
 <p>就活攻略</p>
-<table border="10" width="550" cellspacing="10" cellpadding="5" bordercolor="#ffffff">
+<table border="10" width="500" cellspacing="5" cellpadding="5" bordercolor="#ffffff">
 <tr>
-<th bgcolor="#fff4ea" width="250" height="100"><font color="#FFFFFF">メニュー</font></th>
-<th bgcolor="#fff4ea" width="250" height="100"><font color="#FFFFFF">説明</font></th>
+<th bgcolor="#fff4ea" width="250" valign="top"><font color="#000000">メニュー</font></th>
+<th bgcolor="#fff4ea" width="250" height="100" valign="top"><font color="#000000">説明</font></th>
 </tr>
 <tr>
-<td bgcolor="#fff4ea" align="right" >カルボナーラ</td>
+<td bgcolor="#fff4ea" valign="top" >カルボナーラ</td>
 <td bgcolor="#fff4ea" valign="top" width="250" height="100">玉子とベーコンとクリームソースのパスタ</td>
 </tr>
 <tr>
-<td bgcolor="#fff4ea" align="right" nowrap>ペスカトーレ</td>
+<td bgcolor="#fff4ea" valign="top" >ペスカトーレ</td>
 <td bgcolor="#fff4ea" valign="top" width="250" height="100">エビとあさりの漁師風パスタ</td>
 </tr>
 </table>
